1b7ed9d4ce6a0f2cc2be8a22aaf1b5f2b34b4fbe
omurodjon <famiylinfomy@gmail.com>
2024-03-18 16:14:27 +0500

    ci: deleted in form add isFavorite

--- src/pages/Add/createContact.jsx
+++ src/pages/Add/createContact.jsx
@@ -58,7 +58,7 @@ function CreateContact() {
 					required
 				/>
 			</label>
-			<label>
+			{/* <label>
 				Is Favorite:
 				<input
 					className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-56 p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -68,7 +68,7 @@ function CreateContact() {
 					onChange={(e) => setIsFavorite(e.target.checked)}
 					required
 				/>
-			</label>
+			</label> */}
 			<label>
 				CreatedAt:
 				<input
